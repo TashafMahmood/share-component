7c2eea9c46a8b1765aa171dde9d5e103396e0409
Bishes87 <bishes.sahaofficial@gmail.com>
2023-02-10 12:39:54 +0530

    random string api added

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({
+        img: 'https://random.imagecdn.app/500/150',
+        title: makeid(20),
+        summary: makeid(100),
+    })
+}
+
+function makeid(length) {
+    let result = ''
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    let counter = 0
+    while (counter < length) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength),
+        )
+        counter += 1
+    }
+    return result
 }
--- src/pages/blogs/[id].js
+++ src/pages/blogs/[id].js
@@ -5,59 +5,45 @@ function BlogsDetails(props) {
     const {data} = props
 
     console.log(data)
-  return (
-      <div>
-          <Head>
-              {/* <meta http-equiv='Cache-Control' content='public' />
+    return (
+        <div>
+            <Head>
+                {/* <meta http-equiv='Cache-Control' content='public' />
               <meta http-equiv='Cache-Control' content='max-age=1' />
               <meta http-equiv='Cache-Control' content='must-revalidate' />
               <meta httpEquiv='Pragma' content='no-cache' />
               <meta httpEquiv='Expires' content='0' /> */}
-              .
-              <meta
-                  property='og:title'
-                  content={data?.name ?? ''}
-                  key='title'
-              />
-              <meta
-                  property='og:image'
-                  content={`https://random.imagecdn.app/500/150?a=${Math.random()}`}
-                  key='image'
-              />
-              <meta property='og:description' content={data?.summary ?? ''} />
-          </Head>
-          <div>
-              <div className='card m-2 col-4'>
-                  <img
-                      className='card-img-top'
-                      src={data?.show?.image?.medium ?? ''}
-                      alt='Card image cap'
-                      height={300}
-                  />
-                  <div className='card-body'>
-                      <h5 className='card-title'>{data?.show?.name ?? ''}</h5>
-                      <div
-                          dangerouslySetInnerHTML={{
-                              __html: data?.show?.summary ?? '',
-                          }}></div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  )
+                .
+                <meta
+                    property='og:title'
+                    content={data?.title ?? ''}
+                    key='title'
+                />
+                <meta property='og:image' content={data.img} key='image' />
+                <meta property='og:description' content={data?.summary ?? ''} />
+            </Head>
+            <div className='d-flex align-item-center justify-content-center'>
+                <iframe
+                    src='https://www.elred.io'
+                    width={300}
+                    height={600}
+                    title='W3Schools Free Online Web Tutorials'></iframe>
+            </div>
+        </div>
+    )
 }
 
 export async function getServerSideProps(context) {
-  // console.log(context.query);
-  const { query } = context;
+    // console.log(context.query);
+    const {query} = context
 
-  const res = await fetch(`https://random.imagecdn.app/500/150`)
-  const data = await res.json();
+    const res = await fetch(`https://share-component.vercel.app/api/hello`)
+    const data = await res.json()
 
-  console.log(data);
+    console.log(data)
 
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
+    return {
+        props: {data}, // will be passed to the page component as props
+    }
 }
 export default BlogsDetails;
