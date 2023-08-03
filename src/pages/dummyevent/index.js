a49a96dff9fb93fab5668bfe68046c9031cd33da
umesh kukreti <36157532+umeshckukreti@users.noreply.github.com>
2023-08-03 21:18:01 +0530

    dummy events added

--- src/pages/dummyevent/index.js
+++ src/pages/dummyevent/index.js
@@ -19,12 +19,15 @@ function Dummyevent(props) {
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="300" />
         <meta property="og:image:height" content="300" />
-        <meta property="og:image" content={"/events/1.png"} key="image" />
-        <meta property="og:title" content={"dummy events" ?? ""} key="title" />
         <meta
-          property="og:description"
-          content={"dummy events description" ?? ""}
+          property="og:image"
+          content={
+            "/events/screenshots_63aad78bb38aa1d755b495611691057330867.png"
+          }
+          key="image"
         />
+        <meta property="og:title" content={"dummy events"} key="title" />
+        <meta property="og:description" content={"dummy events description"} />
       </Head>
       <div className="d-flex flex-column align-items-center justify-content-center height-100">
         <div
