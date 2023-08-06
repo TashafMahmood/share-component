6ea82e63fc44fadd73dec82bfbbd670a640477a7
umesh kukreti <36157532+umeshckukreti@users.noreply.github.com>
2023-08-06 22:24:36 +0530

    og title and description changed

--- src/pages/testEvent/index.js
+++ src/pages/testEvent/index.js
@@ -16,8 +16,17 @@ function testEvent(props) {
           content={"https://test.elred.io/events/1.png"}
           key="image"
         />
-        <meta property="og:title" content={"dummy events"} key="title" />
-        <meta property="og:description" content={"dummy events description"} />
+        <meta
+          property="og:title"
+          content={"Future Forward Season 3 Episode 1"}
+          key="title"
+        />
+        <meta
+          property="og:description"
+          content={
+            "This is the first meeting of the new YPO year 23-24 to be held at Mumbai. Kindly be available at St Regis at sharp 6, after which forum rules apply."
+          }
+        />
       </Head>
       <div className="d-flex flex-column align-items-center justify-content-center height-100">
         <iframe
