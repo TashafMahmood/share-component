00d3a80121f0a85f1b3beb9eec92c30104c94a1c
umesh kukreti <36157532+umeshckukreti@users.noreply.github.com>
2023-11-20 22:03:33 +0530

    fix: url changed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const baseURL = "https://dev.elred.io/";
 export const redirectURL = "https://dynamiclinks-9d64a.web.app/";
-export const webviewURL = "http://192.168.1.27:3001/";
+export const webviewURL = "https://demo1.elred.io/";
