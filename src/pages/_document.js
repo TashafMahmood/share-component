908cbcfe2bbb2d803d64eeb7cc5eedffeab5f63c
umesh kukreti <36157532+umeshckukreti@users.noreply.github.com>
2023-11-22 22:18:25 +0530

    fix: zoom scroll issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,12 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
